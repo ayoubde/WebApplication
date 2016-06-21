c29c0b6f713f9a8df21fc70e3db7780215056deb
kit <ayoub.dev@gmail.com>
2016-06-21 15:32:31 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            int[] table = new int[] {1,2,3,4,5 };
+            int count = 0;
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                count++;
+            }
+
+            
+         
+
+            ViewBag.Count = count;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
